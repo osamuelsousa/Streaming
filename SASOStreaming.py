b36e0a719f47e6e02b4e0dfbf98fb0c9ef1c2042
Samuel Sousa <samuelsousa2012k@outlook.com>
2022-06-26 17:14:12 -0300

    Travei na parte de cadastro de uma pessoa, vou estudar e voltar depois de um tempo...

--- SASOStreaming.py
+++ SASOStreaming.py
@@ -1,15 +1,18 @@
 #Criando um passo a passo
 #'''    -Coisas para fazer ainda: login (dar continuidade) / 
-#       -Criar  uma tela de login igual o da disney (botão de entrar, e outro que mostra os planos e depois ja mostra pra pessoa se cadasrtear de acoro com o plano, e um outro de assine agora)
-# '''
+#       -Criar  uma tela de login... (botão de entrar, e outro que mostra os planos e depois ja mostra pra pessoa se cadasrtear de acoro com o plano, e um outro de assine agora)
+# '''   -Travei, pois não consigo criar um sistema onde armazena os dados informados e depois ele pega esses dados para seguir com o login...
 
 from re import S
-
+import random
 
 def formatacao_de_titulos(a):
     print("-"*60)
     print(f'{a:^60}'.upper())
     print("-"*60)
+
+email = ["adm"]
+senhas = ["123"]
 
 #Tela inicial
 iniciando = 0
@@ -17,22 +20,31 @@ while iniciando == 0:
     formatacao_de_titulos("Bem vindo as SASORO Plus")
     print("Digite:\n1 - Para entrar.\n2 - Para se cadastrar.\n3 - Para conhecer os planos.\n4 - Para encerrar o programa.")
     d = int(input("Digite: "))
-
+#Esse é para o login: Status: Não terminado
     if d == 1:
         formatacao_de_titulos("Óla, use seu e-mail para entrar:")
-        email = input("Email: ")
-        senha = input("Senha: ")
+        e = input("E-mail: ")
+        if e in email:
+            s = input("Senha: ")
+        else:
+            print("O e-mail informado não está cadastrado no nosso site!")
+            continue
+        print("entrou")
         #terminar
 
+#Esse é para cadastrar as pessoas: Status: OK
     elif d == 2:
         continuar = 1
         while continuar == 1:
                 formatacao_de_titulos("CADASTRE SUA CONTA ABAIXO.")
-                input("\nInforme um E-mail: ")
+                c = input("\nInforme um E-mail: ")
                 a = input("Crie uma senha: ")
                 b = input("Digite novamente a senha: ")
                 if a == b:
+                    email.append(c)
+                    senhas.append(b)
                     print("\nConta cadastrada com sucesso!")
+                    break
                 elif a != b:
                     b = "As senhas inseridas são diferentes..."
                     c = "Você deseja tentar novamente ou voltar ao inicio?"
@@ -50,10 +62,10 @@ while iniciando == 0:
                         continuar = 0
                     else:
                         print("Erro desconhecido ;(")
-        print("teste")
     elif d == 4:
         break
+
+
 #teste para ver se ele volta ao inicio...             
-num = 0
-while num in range(0 , 100):
-    print(num)
+print("ta chegando no fim")
+print(email, senhas)
--- teste.py
+++ teste.py
@@ -4,6 +4,65 @@
 #else:
 #    print("nao")
 
-num = 0
-while num in range(0 , 100):
-    print(num)
+#num = 0
+#while num in range(0 , 100):
+#    print(num)
+import random
+
+#x = random.randint(1,500)
+#print(x)
+#Resultado: Gera um numero aleatorio de 1 a 500
+
+
+#teste = 0
+#while teste == 0:
+#    a = ""
+#    b = ""
+#    numeroaleatorio = [a,b]
+#    b = numeroaleatorio.append(input("Digite o email: "))
+#    a = numeroaleatorio.append(input("Digite a senha: "))
+#
+#    email = input("Digite seu email: ")
+#    senha = input("Digite sua senha: ")
+#    if email and senha in numeroaleatorio:
+#        if email and senha != "":
+#            print("Você entrou com sucesso")
+#        else:
+#            print("voce digitou o numero magico")
+#    else:
+#        print("erro ao entrar")
+
+
+#print(numeroaleatorio)
+
+
+
+#banco_de_dados = []
+#def novo_usuario():
+#    x = random.randint(1,100000000000) #Numero aleatório até 100Bi de pessoas...
+#    a = ""
+#    b = ""
+#    t = [a,b]
+#    a = t.append(input("Digite o email: "))
+#    b = t.append(input("Digite a senha: "))
+#    print(t)
+#    banco_de_dados.append(x)
+#    print(banco_de_dados)
+#novo_usuario()
+
+#email deve conter a senha dentro... ao digitar o email so vai ter uma senha para entrar, entendeu?
+
+x = []
+a = []
+
+t = random.randint(1,1000000000)
+x.append(input("Digite seu e-mail: "))
+a.append(input("Digite sua senha: "))
+
+print(t)
+t = []
+t.append(x and a)
+print(t)
+
+
+
